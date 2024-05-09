ee2becf62330e3a01ba4e7a8d07bfbca06495af0
aniket kumar verma <85856043+gojosaturo9@users.noreply.github.com>
2024-05-09 13:08:44 +0000

    con. of datatype

--- 01_basics/1_varable.js
+++ 01_basics/1_varable.js
@@ -2,3 +2,6 @@ const accountId = 12233
 let accountEmail = "ajay@gmail.com"
 var accountPassword ="jaipur"
 console.log(accountEmail)
+/*
+do not use var because of issue in block scope and function scope
+*/
