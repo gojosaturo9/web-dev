fd0ab94c686d4dd696014ca3044b0f4c336705bb
aniket kumar verma <85856043+gojosaturo9@users.noreply.github.com>
2024-05-10 14:25:46 +0000

    var

---
+++ 01_basics/operation.js
@@ -0,0 +1,54 @@
+//************operations**************
+//let value = 3
+//let negvaalue = -value
+//console.log(negvaalue)
+
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2)
+
+//let str1 = "hello"
+// let num1, num2, num3 
+
+ 
+//let gamecounter = 100
+//++gamecounter;
+//console.log(gamecounter);
+//console.log(2>1);
+//console.log(2==1);
+
+
+
+
+//variables in javascript
+//variables are container for data
+//name="aniket";
+//console.log(name);
+// null khali
+// undefined mtlb kuch pata nhi kya hai container
+//= is known as assignment operator
+
+
+// varables are case sensitive
+// only  $_ and letters are use even not we used space bewteen the varables
+// only letter _ $ should be first charcter
+// reserved words cannot be variables
+
+
+// var can be re-declared & updated.a global scope variable
+//let cannot be re-declared but updated.a block scope variable
+// const cannot be re-decleared and updated
+
+// data types 
+
+//primtive data type 
+//number 
+//string
+//Boolean
+//undefined
+//null
+//BigInt
+//Symbol
+
