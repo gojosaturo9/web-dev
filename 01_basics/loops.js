1714fe52cdbd3013c3985e6bd4919eee66a629cc
aniket kumar verma <85856043+gojosaturo9@users.noreply.github.com>
2024-05-12 13:33:32 +0000

    loops and strings

---
+++ 01_basics/loops.js
@@ -0,0 +1,52 @@
+ // loops
+ // loops are used to excute code at repeat MediaStreamAudioDestinationNode
+ // for loop
+ // while loop 
+ // forloop
+ /*for( let i = 100; i<= 100; i++){
+    console.log("aniket");
+ }*/
+ /*let sum = 0;
+ for( let i = 1; i<100; i++){
+   sum = sum + i;
+ }
+ console.log("sum=", sum);*/
+/*let i="1";
+while(i <= 5){
+   console.log("i");
+   i++;
+}*/
+
+// do while loop
+/*let i=1;
+do {
+   console.log("i=",i);
+   i++;
+   }   while(i<= 5);*/
+
+// Q> print all even number from 0 to 100.
+/*for( let i = 1; i<= 100; i++){
+   if(i%2!==0)
+   console.log(" i =", i);
+}*/
+
+/*let gameNum = 25;
+let userNum = prompt("guess the number :");
+while(userNum != gameNum
+){
+   userNum = prompt(" you had entered the wrong number. enter the number again :");
+}
+console.log(" congratulations, you have entered the corrrect number");*/
+ 
+/*let fullname  = aniket;
+ prompt( " please enter your username : ");
+let username= "@"+ fullname+fullname.length;
+console.log( username)*/
+
+
+
+
+
+
+
+
---
+++ 01_basics/strings.js
@@ -0,0 +1,26 @@
+// template literals 
+
+// let specailsting = `this is the way of write s template literals ${}`;
+
+// escape character \n is used to change the line
+
+// open tab \t
+
+// strings method 
+//these are built in functions to manipulate a string 
+// str.toUpperCase() change the lower case to upper case return a new value
+// str.toLowerCase()
+// str.trim() removes white space fro the start and end 
+// str.slice(start, end?) 
+// str.concat(str)
+// str. replace(searchVal, newVal)
+// str.charAt(idx)
+
+
+
+
+
+
+
+
+
