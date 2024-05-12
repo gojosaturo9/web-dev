4f20b89152650374664b1cb822a609c74b334e9b
aniket kumar verma <85856043+gojosaturo9@users.noreply.github.com>
2024-05-12 13:51:42 +0000

    arrays

---
+++ 01_basics/arrays.js
@@ -0,0 +1,15 @@
+// arrays collection of items
+//let marks =[ 45, 43, 54, 34, 53];
+//console.log(marks)
+
+//let heroes =["aniket", "abhishek", "jessica","parul"];
+//console.log( heroes)
+
+//looping over Array
+
+//for loop
+
+//for( let idx= 0; idx<Array.length; idx++){
+//
+//}
+
