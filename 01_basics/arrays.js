c0c905ad54e785bcf5ead65d5498bc7a0ad0605c
aniket kumar verma <85856043+gojosaturo9@users.noreply.github.com>
2024-05-13 14:22:00 +0000

    arrays

--- 01_basics/arrays.js
+++ 01_basics/arrays.js
@@ -9,7 +9,71 @@
 
 //for loop
 
-//for( let idx= 0; idx<Array.length; idx++){
-//
+//for( let idx= 0; idx < Arr.length; idx++){
+// console.log(arr[i]);
 //}
+
+/*let cities = ["delhi","bhopal","cheenai","gurgaon","noida","huda city center","cp"]
+for(let city of cities){
+    console.log(city.toUpperCase());
+}*/
+/*let marks = [ 85, 97, 44, 37, 76,60]
+
+let sum = 0;
+for( let val of marks){
+    sum += val;
+}
+let avg = sum/marks.length;
+console.log(`avg marks of the class = ${avg}`);*/
+
+
+/*let items =[250, 645, 300, 900, 50]
+let i=0;
+for(let val of items){
+    let offer = val/10;
+    items[i]=items[i]-offer;
+    console.log(`value after offer= ${items[i]}`);
+    i++;
+}*/
+
+// push() add anything in the end 
+
+// pop() delete anything from the end 
+
+//toString() convert into the string 
+
+// concat() it joins the multiple arrays
+
+//let naruto =["naruto","sasuke","sakura","hinata","sikamaru","itachi","obito","kakashi","madara","minato"]
+//let onepice =["monkey d luffy","roronoa zoro","vinsomke sanji","nico robin","boa hacock","nami","uospp","chopper","brook","jimbe","franky"]
+ //let anime = naruto.concat(onepice);
+ // console.log(anime);
+
+// unshift() it is used to add items from start
+
+// shift() delete any item from start
+
+// slice() return a piece of array
+
+// splice() changes original array( add remove replace)
+
+/*let Arr =[1,2,3,4,5,6,7];
+
+Arr.splice(2,2,101,102);
+// add element
+Arr.splice(2,0,101);
+// delete element
+Arr.splice(3,1)
+// replace element
+Arr.splice(3,1,101);*/
+
+//let companies = ["bloomberg","microsoft","uber","google","ibm","netflix"]
+
+//companies.shift();
+
+//companies.splice(2,1,"ola");
+
+//companies.push("amazon");
+//console.log(companies);
+
 
