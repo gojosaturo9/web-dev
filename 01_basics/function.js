fab1ce9782ca4ad0c211c1dde0034b9e0ab547e1
aniket kumar verma <85856043+gojosaturo9@users.noreply.github.com>
2024-05-14 14:45:58 +0000

    function

---
+++ 01_basics/function.js
@@ -0,0 +1,93 @@
+// function is block of code that performs a specific task, can be invoked whenever needed
+// function can be called any time
+
+// function definition 
+/* function functionName(){
+    //do some work
+}*/
+
+// function call
+
+// functionName();
+
+
+/*function myFunction(){
+    console.log("welcome to bhopal!");
+    console.log("city of lakes");
+}
+
+myFunction();
+myFunction();
+myFunction();
+myFunction();
+myFunction();*/
+
+// arrow function compact way to writing a function 
+
+/* const functionName = (parm1, param2....)=>{
+    do some work
+}
+*/
+
+// foreach loop in arrays
+
+//arr.array.forEach(element => {
+    
+//});
+
+/*let num = [2,3,4,5,6]
+
+nums.forEach((num)=>{
+    console.log(num*num);
+};
+*/
+
+// map method
+
+/*let arr = [1,2,3,4,5,6,7];
+let evenarr = arr.filter((val) =>{
+    return val>3==0;
+});
+console.log(evenarr);*/
+
+// reduce
+
+/*let arr = [5,6,7,3,123,453];
+const output = arr.reduce((prev, curr)=> {
+    return pre> curr ? pre:curr;
+});
+
+console.log(output);*/
+
+
+/*let marks = [99, 43,96,95,54,43,78];
+
+lettoppers=marks.filter((val) => {
+    return val > 90;
+});
+console.log(toppers);*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
