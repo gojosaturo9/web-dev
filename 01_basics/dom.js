7008e7685e533e5183c2d8a5c6bde450a2fd814a
aniket kumar verma <85856043+gojosaturo9@users.noreply.github.com>
2024-05-15 02:45:09 +0000

    dom

---
+++ 01_basics/dom.js
@@ -0,0 +1,37 @@
+// window object
+// the window object represents an open window in a browser. it is browser's object(not javascript's)
+//& is automaticall created by browser.
+
+// selecting with id
+//document.getElementById("myid")
+
+// selecting with class
+//document.getElementsByClassName("class")
+
+//selecting with tag
+//document.getElementsByTagName("mytag")
+
+// querry selector
+//document.querySelector("")
+//document.querySelectorAll("")
+
+//class always used in querry selector with (.)
+
+//properties
+//tagName:- returns tag for element nodes
+//innerText:- returns the text content of the element and all its children
+// innerHTML:- returns the plain text with HTML contents in the element
+// textContent:- returns textual content even for hidden elements
+
+/*let h2 = document.querySelector("h2");
+console.dir(h2.innerText);
+h2.innerText=h2.innerText+"from physics wallah";*/
+
+/*let divs = document.querySelectorAll(".box");
+
+let idx =1;
+for (div of divs){
+   div of divs =`new unique value ${idx}`;
+    idx++;
+}*/
+
