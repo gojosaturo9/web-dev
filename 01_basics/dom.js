889c7018bdf8da9eb50757ccd8703f06ce0a3bfb
aniket kumar verma <85856043+gojosaturo9@users.noreply.github.com>
2024-05-15 13:38:19 +0000

    event

--- 01_basics/dom.js
+++ 01_basics/dom.js
@@ -1,37 +1,61 @@
-// window object
-// the window object represents an open window in a browser. it is browser's object(not javascript's)
-//& is automaticall created by browser.
+/* window objec the window object represents an open window in a browser. it is browser's object(not javascript's)
+& is automaticall created by browser.
 
-// selecting with id
-//document.getElementById("myid")
+ selecting with id
+document.getElementById("myid")
 
-// selecting with class
-//document.getElementsByClassName("class")
+ selecting with classdocument.getElementsByClassName("class")
 
-//selecting with tag
-//document.getElementsByTagName("mytag")
+selecting with tag
+document.getElementsByTagName("mytag")
 
-// querry selector
-//document.querySelector("")
-//document.querySelectorAll("")
+ querry selector
+document.querySelector("")
+document.querySelectorAll("")
+class always used in querry selector with (.)
 
-//class always used in querry selector with (.)
+propertiestagName:- returns tag for element nodesinnerText:- returns the text content of the element and all its children
+ innerHTML:- returns the plain text with HTML contents in the element
+ textContent:- returns textual content even for hidden elements
 
-//properties
-//tagName:- returns tag for element nodes
-//innerText:- returns the text content of the element and all its children
-// innerHTML:- returns the plain text with HTML contents in the element
-// textContent:- returns textual content even for hidden elements
-
-/*let h2 = document.querySelector("h2");
+let h2 = document.querySelector("h2");
 console.dir(h2.innerText);
-h2.innerText=h2.innerText+"from physics wallah";*/
+h2.innerText=h2.innerText+"from physics wallah";
 
-/*let divs = document.querySelectorAll(".box");
+let divs = document.querySelectorAll(".box");
 
 let idx =1;
 for (div of divs){
    div of divs =`new unique value ${idx}`;
     idx++;
-}*/
+}
 
+ attributes  
+getAttribute(Attr)
+
+setAttribute(attr,value)
+
+Node.style to give some style
+
+div.style.backgroundColor= "green";
+
+ exmaple 2  div.style.textSize ="";
+
+ insert elements
+
+ Node.append(el) adds at the end of the Node(inside)
+
+Node.prepend(el) adds at the start of the Node(inside)
+
+Node.before(el) adds before the NodeList(outside)
+
+Node.after(el) adds after the nodes (outside)
+
+
+let element = document.createElement("button");
+    element.innerText="click me !";  document.querySelector("body").prepend("h1");/*
+
+     delete element 
+    node.remove() to remove the nodes
+
+     append child and remove child mdn documentation*/
---
+++ 01_basics/events.js
@@ -0,0 +1,18 @@
+//change in the state of an object is known as event 
+// event are fired to notify code of " intersting change" that may affect code execution
+
+// event handling 
+
+//let btn = document.querySelector("btn");
+//Node.onclick();
+
+//btn1.addEventlistener("click",()=>{
+//    console.log("button1 was clicked-hander2")
+//})
+
+//Node.removeEventlistener(event calllback);
+
+
+
+
+
